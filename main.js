f5b4da055e4cf10105e871d4385a45eff8e85184
unknown <Scampbell@spartaglobal.com>
2018-07-02 17:44:26 +0100

    GAME: Created the player section

---
+++ main.js
@@ -0,0 +1,105 @@
+var playerScore = 0;
+var comScore = 0;
+var comChoice = ["ROCK","PAPER","SCISSORS"];
+alert("<Welcome To The Rock Paper Scissors Game>");
+
+while (player !== 3 || computer !== 3)
+{
+choice = parseString(prompt("What would you like pick: (1)-Rock (2)-Paper (3)-Scissors"))
+
+switch (choice)
+{
+  case 1:
+  pChoice = "ROCK";
+    break;
+
+  case 2:
+  pChoice = "PAPER"
+    break;
+
+  case 3:
+  pChoice = "SCISSORS"
+    break;
+
+  default:
+
+alert("<ERROR> Please Enter Correct Value");
+}
+
+var rand = comChoice[Math.floor(Math.random() * comChoice.length)];
+cChoice = rand;
+
+gamePlay(pChoice, cChoice);
+
+function gamePlay(pChoice, cChoice)
+{
+
+if (pChoice == "ROCK" && cChoice == "SCISSORS")
+{
+  playerScore++;
+  alert("<RockPaperScissors> Player Beats Computer");
+  alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice + " - Player Score: " + playerScore + " | Computer Score " + comScore)
+
+}
+
+else if (pChoice == "ROCK" && cChoice == "PAPER")
+{
+  comScore++;
+  alert("<RockPaperScissors> Computer Beats Player");
+  alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice " - Player Score: " + playerScore + " | Computer Score " + comScore)
+
+}
+
+else if (pChoice == "PAPER" && cChoice == "SCISSORS")
+{
+  comScore++;
+  alert("<RockPaperScissors> Computer Beats Player");
+  alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice " - Player Score: " + playerScore + " | Computer Score " + comScore)
+
+}
+
+else if (pChoice == "SCISSORS" && cChoice == "PAPER")
+{
+  playerScore++;
+  alert("<RockPaperScissors> Player Beats Computer");
+  alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice " - Player Score: " + playerScore + " | Computer Score " + comScore)
+
+}
+
+else if (pChoice == "SCISSORS" && cChoice == "ROCK")
+{
+  comScore++;
+  alert("<RockPaperScissors> Computer Beats Player");
+  alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice " - Player Score: " + playerScore + " | Computer Score " + comScore)
+
+}
+
+else if (pChoice == "PAPER" && cChoice == "ROCK")
+{
+  pScore++;
+  alert("<RockPaperScissors> Player Beats Computer");
+  alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice " - Player Score: " + playerScore + " | Computer Score " + comScore)
+
+}
+else
+
+pChoice === cChoice
+
+alert("<RockPaperScissors> Player AND Computer Has DRAW");
+alert("<RockPaperScissors> Player has Selected " + pChoice + " | Computer has Selected " + cChoice " - Player Score: " + playerScore + " | Computer Score " + comScore);
+
+}
+
+return gamePlay(pChoice, comChoice);
+
+}
+
+If (pScore == 3)
+{
+  alert("Player Has Won The Game");
+}
+
+else if (comScore == 3)
+{
+  alert("Computer Has Won The Game");
+}
